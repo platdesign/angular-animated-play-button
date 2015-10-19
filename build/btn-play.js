616d0f8b6cd43b11a87dea5f1de082cbb3396f63
Christian Blaschke <mail@platdesign.de>
2015-10-19 22:15:23 +0200

    build

--- build/btn-play.js
+++ build/btn-play.js
@@ -1,11 +1,11 @@
 /**
  *
- *	@version	0.0.0
+ *	@version	0.0.1
  *	@author		Christian Blaschke <mail@platdesign.de>
- *	@updated	2015-10-19 20:05:31
+ *	@updated	2015-10-19 20:15:12
  *
  *	Based on the fabulous work of @LeaVerou =)
  *	http://dabblet.com/gist/d21d495d7ce2e5579916
  *
  */
-!function n(r,t,e){function i(u,a){if(!t[u]){if(!r[u]){var f="function"==typeof require&&require;if(!a&&f)return f(u,!0);if(o)return o(u,!0);var l=new Error("Cannot find module '"+u+"'");throw l.code="MODULE_NOT_FOUND",l}var c=t[u]={exports:{}};r[u][0].call(c.exports,function(n){var t=r[u][1][n];return i(t?t:n)},c,c.exports,n,r,t,e)}return t[u].exports}for(var o="function"==typeof require&&require,u=0;u<e.length;u++)i(e[u]);return i}({1:[function(n,r){"use strict";var t=n(1),e=r.exports=t.module("pd.btn-play",[]);e.directive("btnPlay",function(){return{restrict:"A",link:function(n,r,t){function e(n){n?r.addClass("btn-play-playing"):r.removeClass("btn-play-playing")}r.addClass("btn-play"),n.$watch(function(){return n.$eval(t.btnPlay)},function(n,r){n!==r&&e(n)})}}})},{1:"angular"}]},{},[1]);
+!function n(e,t,r){function i(u,a){if(!t[u]){if(!e[u]){var f="function"==typeof require&&require;if(!a&&f)return f(u,!0);if(o)return o(u,!0);var l=new Error("Cannot find module '"+u+"'");throw l.code="MODULE_NOT_FOUND",l}var c=t[u]={exports:{}};e[u][0].call(c.exports,function(n){var t=e[u][1][n];return i(t?t:n)},c,c.exports,n,e,t,r)}return t[u].exports}for(var o="function"==typeof require&&require,u=0;u<r.length;u++)i(r[u]);return i}({1:[function(n,e){(function(n){"use strict";var t=n.angular,r=e.exports=t.module("pd.btn-play",[]);r.directive("btnPlay",function(){return{restrict:"A",link:function(n,e,t){function r(n){n?e.addClass("btn-play-playing"):e.removeClass("btn-play-playing")}e.addClass("btn-play"),n.$watch(function(){return n.$eval(t.btnPlay)},function(n,e){n!==e&&r(n)})}}})}).call(this,"undefined"!=typeof global?global:"undefined"!=typeof self?self:"undefined"!=typeof window?window:{})},{}]},{},[1]);
